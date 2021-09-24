6f43304d715f743b33aee81e8f01cfedc3ef406d
Ishant <ishantchauhan710@gmail.com>
2021-09-24 21:03:23 +0530

    Fragments for Journey, Statistics and Tracking Added

---
+++ app/src/main/java/com/ishant/bikerush/ui/fragments/StatisticsFragment.kt
@@ -0,0 +1,14 @@
+package com.ishant.bikerush.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ishant.bikerush.R
+
+class StatisticsFragment: Fragment(R.layout.fragment_statistics) {
+
+
+
+}

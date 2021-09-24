b33130b853229ba46ede5f0da2cd91f98972acf1
Ishant <ishantchauhan710@gmail.com>
2021-09-24 23:02:05 +0530

    MapView Implemented

--- app/src/main/java/com/ishant/bikerush/ui/TrackingActivity.kt
+++ app/src/main/java/com/ishant/bikerush/ui/TrackingActivity.kt
@@ -2,13 +2,63 @@ package com.ishant.bikerush.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.google.android.gms.maps.GoogleMap
 import com.ishant.bikerush.R
+import com.ishant.bikerush.databinding.ActivityTrackingBinding
 
 class TrackingActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityTrackingBinding
+
+    private var map: GoogleMap? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tracking)
+        binding = ActivityTrackingBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
 
+        binding.mapView.onCreate(savedInstanceState)
 
+        binding.mapView.getMapAsync {
+            map = it
+        }
     }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView?.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView?.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView?.onSaveInstanceState(outState)
+    }
+
+
 }
